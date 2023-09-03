8f3a3a8e4a373845a823cba28f4637f4eb89e938
shengye2413 <2910294488@qq.com>
2023-09-03 22:34:02 +0800

    实现激光移动和清除

--- code/main.c
+++ code/main.c
@@ -83,6 +83,7 @@ int main()
                         if(result==0)
                         {
                             our_air();
+                            //usleep(500);
                             pthread_t tid;
                             pthread_create(&tid,NULL,ball_track,NULL);
                             pthread_join(tid,NULL);
--- code/own_side.c
+++ code/own_side.c
@@ -1,7 +1,9 @@
 #include <pthread.h>
+#include <stdio.h>
 #include "background.h"
 #include "own_side.h"
 #include "slide.h"
+int last_x=80,last_y=240;
 
 //我方飞机
 void *our_air()
@@ -18,29 +20,23 @@ void *our_air()
 void *ball_track()
 {
     //定义last保存上一条激光位置
-    static int last_x=0;
-    static int last_y=0;
-    int x=end_x+80;
-    int y=end_y+50;
-    //画出激光
+    int x=last_x+80;
+    int y=last_y+50;
+    //删除原激光
     while(x<750)
     {
-        draw_pixel(x,y,0xff00ff);
+        draw_pixel(x,y,0x000000);
         x++;
     }
-    //删除原激光
-    if(y!=last_y)
+    //画新激光
+    int i=end_x+80;
+    while(i<750)
     {
-        while(last_x<750)
-        {
-            draw_pixel(last_x,last_y,0x000000);
-            last_x++;
-        }
-        last_x=x;
-        last_y=y;
-        pthread_exit(0);
+        draw_pixel(i,end_y+50,0xff00ff);
+        i++;
     }
     //保存上一条激光位置
-
-    
+    last_x=end_x;
+    last_y=end_y;
+    pthread_exit(0);
 }
